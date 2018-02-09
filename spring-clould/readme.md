8ce13429a16a21679580b94e9928637831e23434
tinaleios <tianleios@163.com>
2018-02-09 17:01:54 +0800

    123

---
+++ spring-clould/readme.md
@@ -0,0 +1,14 @@
+##1. 启动 discovery
+访问`http://localhost:8081/`
+
+
+##2. 启动 service,服务0
+访问`http://localhost:8082/`
+##3. 启动 service2
+访问`http://localhost:8083/`
+
+##4. 启动 gateway
+
+##5.
+`http://localhost:8084/service/service`访问 `服务1`
+`http://localhost:8084/service2/service2`访问 `服务2`
---
+++ spring-clould/service2/src/main/java/Service2Application.java
@@ -0,0 +1,22 @@
+package service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class Service2Application {
+
+    @RequestMapping("/service2")
+    public String service2(){
+        return "this is service2";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service2Application.class, args);
+    }
+}
