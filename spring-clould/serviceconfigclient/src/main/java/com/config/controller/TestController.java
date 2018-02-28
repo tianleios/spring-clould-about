9be88734f0a6f434d293d20fa9cb1811da8a39fe
tinaleios <tianleios@163.com>
2018-02-28 17:15:16 +0800

    push config

--- spring-clould/serviceconfigclient/src/main/java/com/config/controller/TestController.java
+++ spring-clould/serviceconfigclient/src/main/java/com/config/controller/TestController.java
@@ -2,7 +2,7 @@ package com.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
@@ -12,7 +12,7 @@ class TestController {
     @Value("${from}")
     private String from;
 
-    @RequestMapping("/from")
+    @GetMapping("/from")
     public String from() {
 
         return this.from;
