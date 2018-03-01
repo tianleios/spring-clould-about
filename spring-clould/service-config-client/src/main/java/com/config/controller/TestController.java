87d80f11706dce6b525ae83835b9f198af2ec38f
tinaleios <tianleios@163.com>
2018-03-01 19:44:32 +0800

    配置不成功

--- spring-clould/service-config-client/src/main/java/com/config/controller/TestController.java
+++ spring-clould/service-config-client/src/main/java/com/config/controller/TestController.java
@@ -1,11 +1,10 @@
 package com.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
+//@RefreshScope
 @RestController
 class TestController {
 
--- spring-clould/service-config/src/main/java/com/config/ConfigApplication.java
+++ spring-clould/service-config/src/main/java/com/config/ConfigApplication.java
@@ -9,6 +9,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigApplication {
 //    tianlei-dev.properties  url中tianlei，dev  对应 tianlei-dev
 //  启动后 访问：http://localhost:7070/tianlei/dev
+//    http://localhost:7070/tianlei/default
+//    http://localhost:7070/tianlei/test
+//    http://localhost:7070/tianlei/prod
     public static void main(String[] args) {
 
         SpringApplication.run(ConfigApplication.class,args);
